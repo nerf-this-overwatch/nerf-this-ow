f773a52bef415ddf8d93c725c0c07e59b3d2c021
LoicGoyet <loic.goyet@gmail.com>
2019-01-20 15:49:20 +0100

    :recycle: Remove useless component

--- src/components/MatchLine/stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-
-import MatchLine from '.';
-
-const stories = storiesOf('MatchLine', module);
-
-stories.add('Default', () => <MatchLine />);
